4ee36ec575af0a9421b5e1a409670f5698ce67f9
chelyn2008 <chellynlee2005@gmail.com>
2026-09-15 16:09:30 +0800

    changes to experiment file

--- Project1/experiment.py
+++ Project1/experiment.py
@@ -1,9 +1,28 @@
+import random
 from hybrid_sort import HybridMergeSort
 
-A = [7, 4, 5, 2, 8, 1, 6, 3]
-S = 2
+# # generating the data
+# def generate_data(n, x):
+#     A = [] # start with empty list
+#     for _ in range(n):
+#         A.append(random.randint(1, x))
+#     return A
 
+# # number of data to be iterated
+# n_values = [1000, 10000, 100000, 1000000, 10000000]
+# # range of values
+# x = 1000 
+# # threshold value
+# S = 2
+# for n in n_values:
+#     A = generate_data(n, x)
+
+# test on small array
+A = [4, 5, 9, 0, 8, 2, 6, 1, 7, 3]
+S = 2
 comparisons = HybridMergeSort(A, 0, len(A) - 1, S)
 
+
 print(A)
-print(comparisons)
+# print("n =", n, "length =", len(A))
+print("Key comparisons:", comparisons)
